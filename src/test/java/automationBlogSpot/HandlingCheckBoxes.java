5ae23b48573cbba016c5799df16db8c0cc7d1cf6
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-29 09:42:24 +0530

    automationBlogSpot Final Commit

--- src/test/java/automationBlogSpot/HandlingCheckBoxes.java
+++ src/test/java/automationBlogSpot/HandlingCheckBoxes.java
@@ -16,7 +16,7 @@ import org.testng.annotations.Test;
  * 1) Selecting custom check box
  * 2) Selecting all Check Boxes at once
  * 3) Selecting only last three check boxes
- * 4) Selecting only Starting three checkBoxes
+ * 4) Selecting even placed checkBoxes
  * 5) UnSelect the selected checkBoxes*/
 
 public class HandlingCheckBoxes {
@@ -33,47 +33,68 @@ public class HandlingCheckBoxes {
 		// Initiate JavaScrip Executer
 		jse = (JavascriptExecutor) driver;
 	}
-	
+
 	@Test(priority = 1)
 	public void customCheckBox() {
 		// Approach 1
 		WebElement box = driver.findElement(By.xpath("//div[@class='blog-posts hfeed']//div[4]//div[1]"));
 		box.click();
-		System.out.println("You clicked : "+box.getText());
-		
+		System.out.println("You clicked : " + box.getText());
+
 		// Approach 2 (Using Java Script Executer)
 //		jse.executeScript("arguments[0].click()", box);
 //		System.out.println("You clicked : "+box.getText());
 	}
-	
+
 	@Test(priority = 2)
 	public void selectAllBoxes() {
-		List<WebElement> checkBoxes = driver.findElements(By.xpath("//input[@class='form-check-input' and @type='checkbox']"));
-		for(WebElement cb : checkBoxes) {
+		List<WebElement> checkBoxes = driver
+				.findElements(By.xpath("//input[@class='form-check-input' and @type='checkbox']"));
+		for (WebElement cb : checkBoxes) {
 			cb.click();
-			System.out.println("You Clicked : "+cb.getAttribute("id"));
+			System.out.println("You Clicked : " + cb.getAttribute("id"));
 		}
 	}
-	
+
 	@Test(priority = 3)
 	public void lastThreeBoxes() {
-		List<WebElement> checkBoxes = driver.findElements(By.xpath("//input[@class='form-check-input' and @type='checkbox']"));
-		for(int i = 4;i<checkBoxes.size();i++) {
+		List<WebElement> checkBoxes = driver
+				.findElements(By.xpath("//input[@class='form-check-input' and @type='checkbox']"));
+		for (int i = 4; i < checkBoxes.size(); i++) {
 			checkBoxes.get(i).click();
-			System.out.println("You Clicked : "+checkBoxes.get(i).getAttribute("id"));
+			System.out.println("You Clicked : " + checkBoxes.get(i).getAttribute("id"));
 		}
 	}
-	
+
 	@Test(priority = 4)
-	public void firstTheeBoxes() {
-		
+	public void evenCheckBoxes() {
+		List<WebElement> checkBoxes = driver
+				.findElements(By.xpath("//input[@class='form-check-input' and @type='checkbox']"));
+		for (int i = 0; i < checkBoxes.size(); i++) {
+			if (i % 2 == 0) {
+				checkBoxes.get(i).click();
+				System.out.println("You Clicked : " + checkBoxes.get(i).getAttribute("id"));
+			}
+		}
 	}
-	
+
 	@Test(priority = 5)
-	public void unselectSelectedBoxes() {
-		
+	public void unselectSelectedBoxes() throws Exception {
+		List<WebElement> checkBoxes = driver
+				.findElements(By.xpath("//input[@class='form-check-input' and @type='checkbox']"));
+		for (int i = 0; i < checkBoxes.size(); i++) {
+			if (i % 2 != 0) {
+				checkBoxes.get(i).click();
+			}
+		}
+		Thread.sleep(3000);
+		for (int i = 0; i < checkBoxes.size(); i++) {
+			if (checkBoxes.get(i).isSelected()) {
+				checkBoxes.get(i).click();
+			}
+		}
 	}
-	
+
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
