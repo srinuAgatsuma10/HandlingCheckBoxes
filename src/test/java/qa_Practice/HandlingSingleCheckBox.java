e868c27c8e4f1cdcb0fa769c441b30d7139ae4aa
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-04-04 22:01:44 +0530

    Single CheckBox

--- src/test/java/qa_Practice/HandlingSingleCheckBox.java
+++ src/test/java/qa_Practice/HandlingSingleCheckBox.java
@@ -2,7 +2,10 @@ package qa_Practice;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -10,6 +13,7 @@ import org.testng.annotations.Test;
 
 public class HandlingSingleCheckBox {
 	WebDriver driver;
+	JavascriptExecutor jse;
 
 	@BeforeClass
 	public void setUp() {
@@ -18,11 +22,25 @@ public class HandlingSingleCheckBox {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.qa-practice.com/elements/checkbox/single_checkbox");
 		driver.manage().window().maximize();
+		// Initiate Java Script Executor
+		jse = (JavascriptExecutor) driver;
 	}
 	
 	@Test
 	public void singleCheckBox() {
+		// Approach One
+		WebElement checkBox = driver.findElement(By.xpath("//input[@class='form-check-input']"));
+		if(checkBox.isSelected()) {
+			System.out.println(checkBox.getAttribute("value")+" Box is already Selected");
+		}
+		else {
+			checkBox.click();
+			System.out.println(checkBox.getAttribute("value")+" Box is Selected now.");
+		}
 		
+		// Approach Two
+//		jse.executeScript("arguments[0].click()", checkBox);
+//		System.out.println(checkBox.getAttribute("value")+" Box is Selected now.");
 	}
 	
 	@AfterClass
