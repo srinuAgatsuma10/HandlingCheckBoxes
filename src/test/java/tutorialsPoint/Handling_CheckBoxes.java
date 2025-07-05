bf8a8dd18ad1c88aa95d7c5931e01d26b0684ace
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-07-05 18:45:22 +0530

    TestCase Setup

--- src/test/java/tutorialsPoint/Handling_CheckBoxes.java
+++ src/test/java/tutorialsPoint/Handling_CheckBoxes.java
@@ -17,13 +17,32 @@ public class Handling_CheckBoxes {
 		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("");
+		driver.get("https://www.tutorialspoint.com/selenium/practice/check-box.php");
 		driver.manage().window().maximize();
 	}
 
-	@Test
-	public void test() {
-
+	// Main level one
+	
+	@Test(priority = 1)
+	public void clickRootCheckboxes1() {
+		
+	}
+	
+	@Test(priority = 2)
+	public void clickLastLevelBoxes1() {
+		
+	}
+	
+	// Main level 2
+	
+	@Test(priority = 3)
+	public void clickRootCheckboxes2() {
+		
+	}
+	
+	@Test(priority = 4)
+	public void clickLastLevelBoxes2() {
+		
 	}
 
 	@AfterClass
