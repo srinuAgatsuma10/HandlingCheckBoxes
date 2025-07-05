aa5c2b8f731b9bb6212f0fece3e117b9f846f69c
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-07-05 20:20:11 +0530

    MainLevel1 First test case

--- src/test/java/tutorialsPoint/Handling_CheckBoxes.java
+++ src/test/java/tutorialsPoint/Handling_CheckBoxes.java
@@ -2,8 +2,11 @@ package tutorialsPoint;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -21,28 +24,34 @@ public class Handling_CheckBoxes {
 		driver.manage().window().maximize();
 	}
 
-	// Main level one
-	
+	// Main level 1
+
 	@Test(priority = 1)
 	public void clickRootCheckboxes1() {
-		
+		driver.findElement(By.xpath("//input[@id='c_bs_1']")).click();
+		driver.findElement(By.xpath("//li[@id='bs_1']//span[@class='plus']")).click();
+		WebElement subLevel1 = driver.findElement(By.xpath("//input[@id='c_bf_1']"));
+		WebElement subLevel2 = driver.findElement(By.xpath("//input[@id='c_bf_2']"));
+		if(subLevel1.isSelected() && subLevel2.isSelected()) {
+			Assert.assertTrue(true);
+		}
 	}
-	
-	@Test(priority = 2)
+
+//	@Test(priority = 2)
 	public void clickLastLevelBoxes1() {
-		
+
 	}
-	
+
 	// Main level 2
-	
-	@Test(priority = 3)
+
+//	@Test(priority = 3)
 	public void clickRootCheckboxes2() {
-		
+
 	}
-	
-	@Test(priority = 4)
+
+//	@Test(priority = 4)
 	public void clickLastLevelBoxes2() {
-		
+
 	}
 
 	@AfterClass
